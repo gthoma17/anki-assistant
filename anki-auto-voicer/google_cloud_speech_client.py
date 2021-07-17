b116572bd0de8537d6766ff10ef1798679337ca4
Greg Thomas <gthomas.ctr@kr.af.mil>
2021-07-17 17:27:01 -0600

    Add ability to add / remove cloud generated voice to anki deck

--- anki-auto-voicer/google_cloud_speech_client.py
+++ anki-auto-voicer/google_cloud_speech_client.py
@@ -1,40 +1,44 @@
 import os
 from google.cloud import texttospeech
 
-def main():
-	text_to_speech("Hello World", "Hello, world")
-	
-def _set_credentials():
-	creds_folder = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
-	creds_file = os.path.join(creds_folder, "google_creds.secret.json")
-	os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = creds_file
+
+def __set_credentials():
+    creds_folder = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+    creds_file = os.path.join(creds_folder, "google_creds.secret.json")
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = creds_file
 
 
+def __audio_config():
+    return texttospeech.AudioConfig(
+        audio_encoding=texttospeech.AudioEncoding.MP3
+    )
 
-def _audio_config():
-	return texttospeech.AudioConfig(
-	    audio_encoding=texttospeech.AudioEncoding.MP3
-	)
 
-def _voice_config():
-	return texttospeech.VoiceSelectionParams(
-	    language_code="en-US", ssml_gender=texttospeech.SsmlVoiceGender.NEUTRAL
-	)
+def __voice_config(language_code, gender):
+    return texttospeech.VoiceSelectionParams(
+            language_code=language_code,
+            ssml_gender=gender.for_voice()
+    )
 
-def text_to_speech(text, file_name, voice=_voice_config()): 
-	_set_credentials()
-	client = texttospeech.TextToSpeechClient()
-	response = client.synthesize_speech(
-	    input=texttospeech.SynthesisInput(text=text), 
-	    voice=voice, 
-	    audio_config=_audio_config()
-	)
 
-	# The response's audio_content is binary.
-	with open(f"{file_name}.mp3", "wb") as out:
-	    # Write the response to the output file.
-	    out.write(response.audio_content)
-	    print(f'Audio content written to file "{file_name}.mp3"')
+def __text_to_speech(text, file_name, voice):
+    __set_credentials()
+    client = texttospeech.TextToSpeechClient()
+    audio_config = __audio_config()
+    response = client.synthesize_speech(
+        input=texttospeech.SynthesisInput(text=text),
+        voice=voice,
+        audio_config=audio_config
+    )
 
-if __name__ == '__main__':
-	main()
+    # The response's audio_content is binary.
+    with open(file_name, "wb") as out:
+        # Write the response to the output file.
+        out.write(response.audio_content)
+    return file_name
+
+
+def text_to_speech(text, voice_language, voice_gender, file_name=None):
+    file_name = file_name if file_name is not None else f"{text}.mp3"
+    voice = __voice_config(voice_language, voice_gender)
+    return __text_to_speech(text, file_name, voice)
